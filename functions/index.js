9ac883f6f08aac85f3f919dfe9e888319b62ff64
Glayson <glaysonwow@gmail.com>
2023-02-20 11:36:44 -0300

    First commit

---
+++ functions/index.js
@@ -0,0 +1,116 @@
+require("dotenv").config();
+const functions = require("firebase-functions");
+admin = require("firebase-admin");
+express = require("express");
+cors = require("cors");
+app = express();
+app.use(cors({ origin: true }));
+
+var accessKey = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(accessKey),
+  databaseURL: "https://portfolio-3a3b7.firebaseio.com",
+});
+const db = admin.firestore();
+
+app.get("/hello-world", (req, res) => {
+  console.log(accessKey);
+  return res.status(200).send(accessKey);
+});
+
+// create
+app.post("/api/create", (req, res) => {
+  (async () => {
+    try {
+      await db
+        .collection("items")
+        .doc("/" + req.body.id + "/")
+        .create({ item: req.body.item });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// read item
+app.get("/api/readitem/:item_id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("items").doc(req.params.item_id);
+      let item = await document.get();
+      let response = item.data();
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+// read all
+app.get("/api/readall", (req, res) => {
+  console.log("accessKey: ", accessKey);
+  (async () => {
+    try {
+      let query = db.collection("items");
+      let response = [];
+      await query.get().then((querySnapshot) => {
+        let docs = querySnapshot.docs;
+        for (let doc of docs) {
+          const selectedItem = {
+            id: doc.id,
+            item: doc.data().item,
+          };
+          response.push(selectedItem);
+        }
+      });
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+// update
+app.put("/api/update/:item_id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("items").doc(req.params.item_id);
+      await document.update({
+        item: req.body.item,
+      });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+// delete
+app.delete("/api/delete/:item_id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("items").doc(req.params.item_id);
+      await document.delete();
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+exports.app = functions.https.onRequest(app);
